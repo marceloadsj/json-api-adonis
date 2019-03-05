08b04a85731d45945db14dadd8037c1e13b8ac02
Marcelo Jr <marceloadsj1@gmail.com>
2019-03-05 21:38:45 +0100

    Our middleware will be renamed to ContentNegotiationMiddleware

---
+++ src/middlewares/ContentNegotiationMiddleware.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const UnsupportedMediaTypeException = require("../exceptions/UnsupportedMediaTypeException");
+const NotAcceptableException = require("../exceptions/NotAcceptableException");
+
+const contentType = "application/vnd.api+json";
+
+class ContentNegotiationMiddleware {
+  constructor({ config, JsonApiService }) {
+    this.config = config;
+    this.JsonApiService = JsonApiService;
+  }
+
+  async handle({ request, response }, next) {
+    const requestContentType = request.header("Content-Type").trim();
+
+    if (requestContentType !== contentType) {
+      throw UnsupportedMediaTypeException.invoke(contentType);
+    }
+
+    const accept = request.header("Accept");
+
+    if (accept && accept.includes(`${contentType}`)) {
+      const parsedAccept = accept.split(",").map(accept => accept.trim());
+
+      const isAcceptValid = parsedAccept.some(accept => {
+        if (accept === contentType) return true;
+        return false;
+      });
+
+      if (!isAcceptValid) {
+        throw NotAcceptableException.invoke(contentType);
+      }
+    }
+
+    if (request.hasBody() && this.config.deserializeBody) {
+      const data = request.input("data");
+
+      if (data) {
+        request.body = this.JsonApiService.deserialize(data.type, request.body);
+      }
+    }
+
+    response.type(contentType);
+
+    await next();
+  }
+}
+
+module.exports = ContentNegotiationMiddleware;
