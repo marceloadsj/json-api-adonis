0b2dd8816aefc43755e277149941841a4e0f8e32
Marcelo Jr <marceloadsj1@gmail.com>
2019-03-13 20:52:18 +0100

    Creating query tests in json api service

--- test/unit/json-api-service.spec.js
+++ test/unit/json-api-service.spec.js
@@ -1,4 +1,13 @@
+const path = require("path");
 const test = require("japa");
+const { Config } = require("@adonisjs/sink");
+const DatabaseManager = require("@adonisjs/lucid/src/Database/Manager");
+const iocResolver = require("@adonisjs/lucid/lib/iocResolver");
+const fold = require("@adonisjs/fold");
+
+iocResolver.setFold(fold);
+
+const Model = require("@adonisjs/lucid/src/Lucid/Model");
 
 const JsonApiService = require("../../src/services/JsonApiService");
 
@@ -327,5 +336,257 @@ test.group("JsonApiService", () => {
         }
       ]
     });
+  });
+});
+
+test.group("JsonApiService async", group => {
+  group.before(async () => {
+    fold.ioc.bind("Adonis/Src/Database", () => {
+      const config = new Config();
+
+      config.set("database", {
+        connection: "testing",
+        testing: {
+          client: "sqlite",
+          connection: {
+            filename: path.join(__dirname, "test.sqlite3")
+          }
+        }
+      });
+
+      return new DatabaseManager(config);
+    });
+
+    const Database = fold.ioc.use("Adonis/Src/Database");
+
+    await Promise.all([
+      Database.schema.createTable("users", table => {
+        table.increments();
+        table.string("name");
+        table.string("email");
+        table.string("password");
+        table.boolean("is_active");
+        table.timestamps();
+      }),
+
+      Database.schema.createTable("tokens", table => {
+        table.increments();
+        table
+          .integer("user_id")
+          .unsigned()
+          .references("id")
+          .inTable("users");
+        table.string("token");
+        table.string("type");
+        table.boolean("is_revoked");
+        table.timestamps();
+      })
+    ]);
+
+    await Database.table("users").insert({ name: "marcelo" });
+    await Database.table("tokens").insert({ user_id: 1 });
+  });
+
+  group
+    .after(async () => {
+      const Database = fold.ioc.use("Adonis/Src/Database");
+      await Promise.all([
+        Database.schema.dropTable("users"),
+        Database.schema.dropTable("tokens")
+      ]);
+      Database.close();
+    })
+    .timeout(0);
+
+  test("create simple query without request params", async assert => {
+    const config = {
+      types: { user: { model: "App/Models/User" } }
+    };
+
+    const jsonApiService = new JsonApiService({ config });
+
+    class User extends Model {}
+    User._bootIfNotBooted();
+
+    let jsonApiQuery;
+    User.onQuery(query => {
+      jsonApiQuery = query;
+    });
+
+    await jsonApiService.query(User, {}).first();
+
+    assert.equal(jsonApiQuery.sql, "select * from `users` limit ?");
+  });
+
+  test("create query with fields request param", async assert => {
+    const config = {
+      types: { user: { model: "App/Models/User" } }
+    };
+
+    const jsonApiService = new JsonApiService({ config });
+
+    class User extends Model {}
+    User._bootIfNotBooted();
+
+    let jsonApiQuery;
+    User.onQuery(query => {
+      jsonApiQuery = query;
+    });
+
+    const fields = { user: "name,email" };
+
+    await jsonApiService.query(User, { fields }).first();
+
+    assert.equal(
+      jsonApiQuery.sql,
+      "select `name`, `email`, `id` from `users` limit ?"
+    );
+  });
+
+  test("create query with include request param", async assert => {
+    const config = {
+      types: {
+        tokens: {
+          model: "App/Models/Token",
+          options: {
+            relationships: {
+              user: {
+                type: "users",
+                alternativeKey: "user_id"
+              }
+            }
+          }
+        },
+        users: {
+          model: "App/Models/User"
+        }
+      }
+    };
+
+    const jsonApiService = new JsonApiService({ config });
+
+    class Token extends Model {
+      user() {
+        return this.belongsTo(User);
+      }
+    }
+    class User extends Model {
+      tokens() {
+        return this.hasMany(Token);
+      }
+    }
+    User._bootIfNotBooted();
+    Token._bootIfNotBooted();
+
+    let userQuery;
+    User.onQuery(query => {
+      userQuery = query;
+    });
+
+    let tokenQuery;
+    Token.onQuery(query => {
+      tokenQuery = query;
+    });
+
+    const include = "tokens";
+
+    await jsonApiService.query(User, { include }).first();
+
+    assert.equal(userQuery.sql, "select * from `users` limit ?");
+    assert.equal(
+      tokenQuery.sql,
+      "select * from `tokens` where `tokens`.`user_id` in (?)"
+    );
+  });
+
+  test("create query with fields and include request param", async assert => {
+    const config = {
+      types: {
+        tokens: {
+          model: "App/Models/Token",
+          options: {
+            relationships: {
+              user: {
+                type: "users",
+                alternativeKey: "user_id"
+              }
+            }
+          }
+        },
+        users: {
+          model: "App/Models/User",
+          options: {
+            relationships: {
+              tokens: {
+                type: "tokens",
+                alternativeKey: "token_id"
+              }
+            }
+          }
+        }
+      }
+    };
+
+    const jsonApiService = new JsonApiService({ config });
+
+    class Token extends Model {
+      user() {
+        return this.belongsTo(User);
+      }
+    }
+    class User extends Model {
+      tokens() {
+        return this.hasMany(Token);
+      }
+    }
+    User._bootIfNotBooted();
+    Token._bootIfNotBooted();
+
+    let userQuery;
+    User.onQuery(query => {
+      userQuery = query;
+    });
+
+    let tokenQuery;
+    Token.onQuery(query => {
+      tokenQuery = query;
+    });
+
+    const fields = {
+      users: "name",
+      tokens: "id"
+    };
+    const include = "tokens";
+
+    await jsonApiService.query(User, { fields, include }).first();
+
+    assert.equal(userQuery.sql, "select `name`, `id` from `users` limit ?");
+    assert.equal(
+      tokenQuery.sql,
+      "select `id` from `tokens` where `tokens`.`user_id` in (?)"
+    );
+  });
+
+  test("create query with sort request params", async assert => {
+    const config = {
+      types: { user: { model: "App/Models/User" } }
+    };
+
+    const jsonApiService = new JsonApiService({ config });
+
+    class User extends Model {}
+    User._bootIfNotBooted();
+
+    let jsonApiQuery;
+    User.onQuery(query => {
+      jsonApiQuery = query;
+    });
+
+    await jsonApiService.query(User, { sort: "name" }).first();
+
+    assert.equal(
+      jsonApiQuery.sql,
+      "select * from `users` order by `name` asc limit ?"
+    );
   });
 });
